720f09559a3bd9f45f92769c6e81be0f37d849f5
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 00:50:57 -0400

    Add and style photo of Victor

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,35 @@ const App = () => (
     <div className="h-full bg-transparent bg-[url('/src/assets/bg-pattern-bottom.svg')] bg-[left_bottom_-47rem] bg-no-repeat flex justify-center items-center">
       <div className="bg-white rounded-2xl h-96 w-80">
         <div className="bg-[url('/src/assets/bg-pattern-card.svg')] h-1/3 rounded-t-2xl" />
-        <div className="h-2/3" />
+        <div className="h-2/3 relative">
+          <div className="flex justify-center">
+            <img src="/src/assets/image-victor.jpg" alt="Victor Crest" className="bg-white rounded-full absolute bottom-52 p-1" />
+          </div>
+          <div>
+            <div>
+              <h1>Victor Crest</h1>
+              <p>26</p>
+            </div>
+            <h2>London</h2>
+          </div>
+          <div>
+            <div>
+              <h3>80K</h3>
+              <p>Followers</p>
+            </div>
+            <div>
+              <h3>803K</h3>
+              <p>Likes</p>
+            </div>
+            <div>
+              <h3>1.4K</h3>
+              <p>Photos</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 );
 
 export default App;
-
-{ /* <div>Photo</div>
-        <div>
-          <div>
-            <h1>Victor Crest</h1>
-            <p>26</p>
-          </div>
-          <h2>London</h2>
-        </div>
-        <div>
-          <div>
-            <h3>80K</h3>
-            <p>Followers</p>
-          </div>
-          <div>
-            <h3>803K</h3>
-            <p>Likes</p>
-          </div>
-          <div>
-            <h3>1.4K</h3>
-            <p>Photos</p>
-          </div>
-        </div> */ }
