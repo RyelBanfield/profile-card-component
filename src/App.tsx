9c67cf7c9526e753fb466c1664ca43e9097b1c9a
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-19 21:53:30 -0400

    Customize tailwind and structure HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+// Dark cyan: hsl(185, 75%, 39%)
+// Very dark desaturated blue: hsl(229, 23%, 23%)
+// Dark grayish blue: hsl(227, 10%, 46%)
+// Dark gray: hsl(0, 0%, 59%)
+
 const App = () => (
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <div>
+
+      <div>Photo</div>
+
+      <div>
+        <div>
+          <h1>Victor Crest</h1>
+          <p>26</p>
+        </div>
+        <h2>London</h2>
+      </div>
+
+      <div>
+        <div>
+          <h3>80K</h3>
+          <p>Followers</p>
+        </div>
+
+        <div>
+          <h3>803K</h3>
+          <p>Likes</p>
+        </div>
+
+        <div>
+          <h3>1.4K</h3>
+          <p>Photos</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
 );
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+    },
   },
   plugins: [],
 };
