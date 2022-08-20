0c9aea9ee88f8d2f2a94ceb13e6cc929b6c4f4be
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 00:25:28 -0400

    Position background SVGs

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
-// Dark cyan: hsl(185, 75%, 39%)
-// Very dark desaturated blue: hsl(229, 23%, 23%)
-// Dark grayish blue: hsl(227, 10%, 46%)
-// Dark gray: hsl(0, 0%, 59%)
-
 const App = () => (
-  <div>
-    <div>
+  <div className="h-screen bg-dark-cyan bg-[url('/src/assets/bg-pattern-top.svg')]  bg-[right_bottom_26rem] bg-no-repeat text-very-dark-desaturated-blue">
+    <div className="h-screen bg-transparent bg-[url('/src/assets/bg-pattern-bottom.svg')] bg-[left_bottom_-47rem] bg-no-repeat flex justify-center items-center">
 
-      <div>Photo</div>
+      <div className="bg-white rounded-2xl border h-96 w-80">
 
-      <div>
+        <div>Photo</div>
+
         <div>
-          <h1>Victor Crest</h1>
-          <p>26</p>
-        </div>
-        <h2>London</h2>
-      </div>
-
-      <div>
-        <div>
-          <h3>80K</h3>
-          <p>Followers</p>
+          <div>
+            <h1>Victor Crest</h1>
+            <p>26</p>
+          </div>
+          <h2>London</h2>
         </div>
 
         <div>
-          <h3>803K</h3>
-          <p>Likes</p>
+          <div>
+            <h3>80K</h3>
+            <p>Followers</p>
+          </div>
+
+          <div>
+            <h3>803K</h3>
+            <p>Likes</p>
+          </div>
+
+          <div>
+            <h3>1.4K</h3>
+            <p>Photos</p>
+          </div>
         </div>
 
-        <div>
-          <h3>1.4K</h3>
-          <p>Photos</p>
-        </div>
       </div>
 
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,15 +8,16 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+      },
+    },
     fontFamily: {
       sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
-    },
-    colors: {
-      'dark-cyan': 'hsl(185, 75%, 39%)',
-      'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
-      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
-      'dark-gray': 'hsl(0, 0%, 59%)',
     },
   },
   plugins: [],
