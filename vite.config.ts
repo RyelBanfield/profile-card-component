d23eb2a927992a9c1e7a134b2a027e0876d8c42e
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 21:27:36 -0400

    Remove typechecking from this file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// @ts-nocheck
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 import eslint from '@rollup/plugin-eslint';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
