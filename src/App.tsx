cf2e7490ad96fd561986a6ad6b8421a5ac8bbfea
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 20:47:46 -0400

    Style text of card

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
 const App = () => (
-  <div className="h-screen bg-dark-cyan bg-[url('/src/assets/bg-pattern-top.svg')]  bg-[right_bottom_26rem] bg-no-repeat text-very-dark-desaturated-blue">
+  <div className="h-screen bg-dark-cyan bg-[url('/src/assets/bg-pattern-top.svg')]  bg-[right_bottom_26rem] bg-no-repeat">
     <div className="h-full bg-transparent bg-[url('/src/assets/bg-pattern-bottom.svg')] bg-[left_bottom_-47rem] bg-no-repeat flex justify-center items-center">
       <div className="bg-white rounded-2xl h-96 w-80">
         <div className="bg-[url('/src/assets/bg-pattern-card.svg')] h-1/3 rounded-t-2xl" />
-        <div className="h-2/3 relative">
-          <div className="flex justify-center">
+        <div className="h-2/3 relative font-normal flex flex-col">
+          <div className="flex justify-center mb-16">
             <img src="/src/assets/image-victor.jpg" alt="Victor Crest" className="bg-white rounded-full absolute bottom-52 p-1" />
           </div>
-          <div>
-            <div>
-              <h1>Victor Crest</h1>
-              <p>26</p>
+          <div className="mb-5">
+            <div className="flex justify-center mb-1">
+              <h1 className="font-bold text-very-dark-desaturated-blue mx-1">Victor Crest</h1>
+              <p className="text-dark-grayish-blue mx-1">26</p>
             </div>
-            <h2>London</h2>
+            <h2 className="text-dark-gray text-center">London</h2>
           </div>
-          <div>
+          <div className="border-t" />
+          <div className="grow flex justify-evenly items-center text-center">
             <div>
-              <h3>80K</h3>
-              <p>Followers</p>
+              <h3 className="font-bold text-very-dark-desaturated-blue">80K</h3>
+              <p className="text-dark-gray">Followers</p>
             </div>
             <div>
-              <h3>803K</h3>
-              <p>Likes</p>
+              <h3 className="font-bold text-very-dark-desaturated-blue">803K</h3>
+              <p className="text-dark-gray">Likes</p>
             </div>
             <div>
-              <h3>1.4K</h3>
-              <p>Photos</p>
+              <h3 className="font-bold text-very-dark-desaturated-blue">1.4K</h3>
+              <p className="text-dark-gray">Photos</p>
             </div>
           </div>
         </div>
