dbcfe56920a88e43a72eb85b3d17e84d9ec1b2a5
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 21:01:16 -0400

    Adjust font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ const App = () => (
     <div className="h-full bg-transparent bg-[url('/src/assets/bg-pattern-bottom.svg')] bg-[left_bottom_-47rem] bg-no-repeat flex justify-center items-center">
       <div className="bg-white rounded-2xl h-96 w-80">
         <div className="bg-[url('/src/assets/bg-pattern-card.svg')] h-1/3 rounded-t-2xl" />
-        <div className="h-2/3 relative font-normal flex flex-col">
+        <div className="h-2/3 relative flex flex-col">
           <div className="flex justify-center mb-16">
             <img src="/src/assets/image-victor.jpg" alt="Victor Crest" className="bg-white rounded-full absolute bottom-52 p-1" />
           </div>
           <div className="mb-5">
-            <div className="flex justify-center mb-1">
+            <div className="flex justify-center mb-1 text-lg">
               <h1 className="font-bold text-very-dark-desaturated-blue mx-1">Victor Crest</h1>
               <p className="text-dark-grayish-blue mx-1">26</p>
             </div>
@@ -17,15 +17,15 @@ const App = () => (
           <div className="border-t" />
           <div className="grow flex justify-evenly items-center text-center">
             <div>
-              <h3 className="font-bold text-very-dark-desaturated-blue">80K</h3>
+              <h3 className="font-bold text-very-dark-desaturated-blue text-lg">80K</h3>
               <p className="text-dark-gray">Followers</p>
             </div>
             <div>
-              <h3 className="font-bold text-very-dark-desaturated-blue">803K</h3>
+              <h3 className="font-bold text-very-dark-desaturated-blue text-lg">803K</h3>
               <p className="text-dark-gray">Likes</p>
             </div>
             <div>
-              <h3 className="font-bold text-very-dark-desaturated-blue">1.4K</h3>
+              <h3 className="font-bold text-very-dark-desaturated-blue text-lg">1.4K</h3>
               <p className="text-dark-gray">Photos</p>
             </div>
           </div>
