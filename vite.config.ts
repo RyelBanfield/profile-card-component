a646f028ff57b0e7771c888966e3498a3cdaaf7d
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-20 22:05:54 -0400

    Update base and deploy sh

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/profile-card-component/',
   plugins: [
     { ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }), enforce: 'pre' },
     react(),
   ],
-  base: '/profile-card-component/',
 });
